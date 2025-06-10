8c0403cebd1116422db3eab84d59cc7153de58f2
Stiigma <eduardoby14@gmail.com>
2025-06-10 14:42:41 -0700

    Creacion de Torneo

---
+++ lolhubmexico/Controllers/TorneoController/TorneoController.cs
@@ -0,0 +1,49 @@
+﻿using LolHubMexico.Application.Exceptions;
+using LolHubMexico.Application;
+using LolHubMexico.Domain.DTOs.Teams;
+using Microsoft.AspNetCore.Mvc;
+using LolHubMexico.Application.TorneoServices;
+using LolHubMexico.Domain.DTOs.Torneo;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+
+namespace LolHubMexico.Controllers.TorneoController
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TorneoController : Controller
+    {
+
+        private readonly TorneoService _torneoService;
+
+        public TorneoController(TorneoService teamService)
+        {
+            _torneoService = teamService;
+        }
+
+
+        [HttpPost("create-torneo")]
+
+        public async Task<ActionResult> CreateTeamAsync(TorneoDTO newTeam)
+        {
+            try
+            {
+
+                var createdTeam = await _torneoService.CrearTorneoAsync(newTeam);
+                var created = true;
+                return Ok(new { created, createdTeam });
+            }
+            catch (AppException ex)
+            {
+                // Error personalizado que lanzas desde el servicio
+                return BadRequest(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                // Otro tipo de error no controlado
+                return StatusCode(500, new { message = "Error interno del servidor", detail = ex.Message });
+            }
+        }
+    }
+}
